d74014369194f39412dad383f41bb1be20b3d6a4
Alexey Popov <aleshaka@inbox.ru>
2023-05-29 13:11:33 +0300

    #2

--- news/pytest_tests/conftest.py
+++ news/pytest_tests/conftest.py
@@ -49,106 +49,32 @@ def comment(author, news):
     return comment
 
 
-# @pytest.fixture
-# def sorted_news():
-#     news1 = News.objects.create(
-#         title='Новость 1',
-#         text='Текст новости 1',
-#         date=datetime(2023, 5, 26),
-#     )
-#     news2 = News.objects.create(
-#         title='Новость 2',
-#         text='Текст новости 2',
-#         date=datetime(2023, 5, 27),
-#     )
-#     news3 = News.objects.create(
-#         title='Новость 3',
-#         text='Текст новости 3',
-#         date=datetime(2023, 5, 25),
-#     )
-#     sorted_news = News.objects.order_by('-date')
-#     return sorted_news
-
-
-# @pytest.fixture
-# def sorted_comments(author, news):
-#     # comment1 = Comment.objects.create(
-#     #     text='Комментарий 1',
-#     #     author=author,
-#     #     news=news,
-#     #     created=datetime(2023, 5, 1),
-#     # )
-#     # comment2 = Comment.objects.create(
-#     #     text='Комментарий 2',
-#     #     author=author,
-#     #     news=news,
-#     #     created=datetime(2023, 5, 3),
-#     # )
-#     # comment3 = Comment.objects.create(
-#     #     text='Комментарий 3',
-#     #     author=author,
-#     #     news=news,
-#     #     created=datetime(2023, 5, 2),
-#     # )
-#     sorted_comments = Comment.objects.filter(news=news).order_by('created')
-#     # return sorted_comments
-#     return sorted_comments
-
-# @pytest.fixture
-# def create_comments(news, author):
-#     comments_data = [
-#         {'text': 'Средний комментарий', 'days_ago': 2},
-#         {'text': 'Новый комментарий', 'days_ago': 1},
-#         {'text': 'Старый комментарий', 'days_ago': 3},
-#     ]
-#
-#     comments = []
-#     current_date = datetime.now()
-#
-#     for comment_data in comments_data:
-#         created_date = current_date - timedelta(days=comment_data['days_ago'])
-#         comment = Comment.objects.create(
-#             text=comment_data['text'],
-#             author=author,
-#             news=news,
-#             created=created_date,
-#         )
-#         comments.append(comment)
-#
-#     return comments
-
-
 @pytest.fixture
 def comments(author, news):
     now = datetime.now()
-
     comment1 = Comment.objects.create(
         text='старый Комментарий 1',
         author=author,
         news=news,
         created=now - timedelta(days=3),
     )
-
     comment2 = Comment.objects.create(
         text='средний Комментарий 2',
         author=author,
         news=news,
         created=now - timedelta(days=2),
     )
-
     comment3 = Comment.objects.create(
         text='новый Комментарий 3',
         author=author,
         news=news,
         created=now - timedelta(days=1),
     )
-
     return [comment1, comment2, comment3]
 
 
 @pytest.fixture
 def recent_news(author):
-    # current_date = datetime.now().strftime('%Y-%m-%d')
     now = datetime.now()
     recent_news_1 = News.objects.create(
         title='Свежая новость 1',
